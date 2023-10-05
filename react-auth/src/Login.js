273da7ca81ba7d5d9278d93614626702c7a8fb8f
Zachary Fowler <ZacharySamuel.Fowler@gmail.com>
2023-10-04 21:49:20 -0700

    Fixed updated material and am still trouble shooting getting the react components to appear
    Signed-off-by: Zachary Fowler <ZacharySamuel.Fowler@gmail.com>

--- react-auth/src/Login.js
+++ react-auth/src/Login.js
@@ -1,70 +1,78 @@
 import React, { useState } from 'react';
 import { Form, Button } from "react-bootstrap";
 import axios from 'axios';
+import Cookies from "universal-cookie";
+const cookies = new Cookies();
 
 const Login = () => {
-      const [email, setEmail] = useState("");
-      const [password, setPassword] = useState("");
-      const [login, setLogin] = useState(false);
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [login, setLogin] = useState(false);
 
 
-      const handleSubmit = (e) => {
-        // prevent the form from refreshing the whole page
-        e.preventDefault();
-        // // make a popup alert showing the "submitted" text
-        // alert("Submited");
-        // set configurations
-        const configuration = {
-            method: "post",
-            url: "https://authappbackend-acef5587e328.herokuapp.com/login",
-            data: {
-                email,
-                password,
-            },
-        };
-        // make the API call
-        axios(configuration)
-            .then((result) => {
-                setLogin(true);
-            })
-            .catch((error) => {
-                error = new Error();
-            });
+  const handleSubmit = (e) => {
+    // prevent the form from refreshing the whole page
+    e.preventDefault();
+    // // make a popup alert showing the "submitted" text
+    // alert("Submited");
+    // set configurations
+    const configuration = {
+      method: "post",
+      url: "https://authappbackend-acef5587e328.herokuapp.com/login",
+      data: {
+        email,
+        password,
+      },
+    };
+    // make the API call
+    axios(configuration)
+      .then((result) => {
+        setLogin(true);
+        // set the cookie
+        cookies.set("TOKEN", result.data.token, {
+          path: "/",
+        });
+        // redirect user to the auth page
+        window.location.href = "/auth";
+      })
+      .catch((error) => {
+        error = new Error();
+      });
 
-    }
+  }
   return (
     <>
-        <h2>Login</h2>
-      <Form onSubmit={(e)=>handleSubmit(e)}>
+      <h2>Login</h2>
+      <Form onSubmit={(e) => handleSubmit(e)}>
         {/* email */}
         <Form.Group controlId="formBasicEmail">
           <Form.Label>Email address</Form.Label>
-          <Form.Control 
-          type="email" 
-          placeholder="Enter email" 
-          name='email'
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
+          <Form.Control
+            type="email"
+            placeholder="Enter email"
+            name='email'
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
           />
         </Form.Group>
 
         {/* password */}
         <Form.Group controlId="formBasicPassword">
           <Form.Label>Password</Form.Label>
-          <Form.Control 
-          type="password" 
-          placeholder="Password" 
-          name='password'
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
+          <Form.Control
+            type="password"
+            placeholder="Password"
+            name='password'
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
           />
         </Form.Group>
 
         {/* submit button */}
-        <Button 
-        variant="primary" 
-        type="submit"
-        onClick={(e) => handleSubmit(e)}
+        <Button
+          variant="primary"
+          type="submit"
+          onClick={(e) => handleSubmit(e)}
         >
           Submit
         </Button>
